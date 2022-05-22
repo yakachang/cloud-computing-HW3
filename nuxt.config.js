92de05d656e5fc2b6bf87976058957fb701519df
yichen <yichen@nlplab.cc>
2022-05-22 20:54:59 +0800

    chore: set axios and proxy

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/firebase.js'
+    '~/plugins/firebase.js',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -41,21 +41,32 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/proxy'
   ],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  https: true,
+
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    // proxy: true,
+    prefix: '/api',
+    credentials: true,
+    proxyHeaders: false,
+  },
+
+  proxy: {
+    '/api': {
+      target: 'http://venom.nlplab.cc:7089/', 
+      changeOrigin: true,
+    }
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
-        dark: {
+        light: {
           primary: colors.blue.darken2,
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
