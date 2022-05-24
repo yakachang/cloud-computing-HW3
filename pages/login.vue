47c71834db3e44b4dde144842418f3bb49a76419
yichen <yichen@nlplab.cc>
2022-05-24 16:04:36 +0800

    feat: add login page

---
+++ pages/login.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <v-btn @click="signInWithGoogle">Sign in with Google</v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+  methods: {
+    async signInWithGoogle() {
+      var provider = new this.$fireModule.auth.GoogleAuthProvider();
+      
+      let authData = await this.$fire.auth.signInWithPopup(provider)
+      this.$router.push('/')
+    },
+  },
+}
+</script>
