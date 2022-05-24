2e005ffc010fee41ea97dae52c80bd7634b13ad9
yichen <yichen@nlplab.cc>
2022-05-24 16:50:33 +0800

    style: add google-icon

--- pages/login.vue
+++ pages/login.vue
@@ -1,9 +1,22 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <v-btn @click="signInWithGoogle">Sign in with Google</v-btn>
-    </v-col>
-  </v-row>
+  <v-container fluid class="my-10">
+    <v-layout justify-center>
+      <v-btn
+        @click="signInWithGoogle"
+        x-large
+        tile
+      >
+        <v-img
+          :src="require('~/assets/Google-icon.png')"
+          max-height="35"
+          max-width="35"
+          class="mr-2"
+        ></v-img>
+        <v-spacer></v-spacer>
+        Sign in with Google
+      </v-btn>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
