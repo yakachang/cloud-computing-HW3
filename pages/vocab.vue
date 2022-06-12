4eb504e13247d0898c5034bd62dd753681ae06b6
yichen <yichen@nlplab.cc>
2022-06-12 20:24:35 +0800

    feat: show dialog when completing the vocabs

--- pages/vocab.vue
+++ pages/vocab.vue
@@ -1,81 +1,104 @@
 <template>
-  <v-row class="mt-10">
-    <v-col cols="4">
-      <v-list shaped>
-        <v-subheader class="text-h5 my-5">CEFR level</v-subheader>
-        <v-list-item-group
-          v-model="selectedLevel"
-          color="primary"
-        >
-          <v-list-item
-            v-for="(item, i) in items"
-            :key="i"
+  <div>
+    <v-row class="mt-10">
+      <v-col cols="4">
+        <v-list shaped>
+          <v-subheader class="text-h5 my-5">CEFR level</v-subheader>
+          <v-list-item-group
+            v-model="selectedLevel"
+            color="primary"
           >
-            <v-list-item-icon>
-              <v-icon v-text="item.icon"></v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title
-                v-text="item.text"
-                class="text-h6"
-              ></v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-col>
-    <v-col cols="8">
-      <v-card
-        class="mx-auto"
-      >
-        <v-system-bar
-          color="pink darken-2"
-          dark
+            <v-list-item
+              v-for="(item, i) in items"
+              :key="i"
+            >
+              <v-list-item-icon>
+                <v-icon v-text="item.icon"></v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title
+                  v-text="item.text"
+                  class="text-h6"
+                ></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-col>
+      <v-col cols="8">
+        <v-card
+          class="mx-auto"
         >
-          <v-spacer></v-spacer>
-          <v-icon>mdi-window-minimize</v-icon>
-          <v-icon>mdi-window-maximize</v-icon>
-          <v-icon>mdi-close</v-icon>
-        </v-system-bar>
+          <v-system-bar
+            color="pink darken-2"
+            dark
+          >
+            <v-spacer></v-spacer>
+            <v-icon>mdi-window-minimize</v-icon>
+            <v-icon>mdi-window-maximize</v-icon>
+            <v-icon>mdi-close</v-icon>
+          </v-system-bar>
 
-        <v-app-bar
+          <v-app-bar
+            dark
+            color="pink"
+          >
+            <v-toolbar-title>Vocabularies ({{cardIdx+1}}/{{boundMapper[selectedLevel]}})</v-toolbar-title>
+          </v-app-bar>
+
+          <v-container>
+            <v-row class="justify-center">
+              <v-col cols="12">
+                <v-card
+                  class="ma-5"
+                >
+                  <v-card-title class="text-h4">{{ vocabs[cardIdx]['headword'] }}</v-card-title>
+                  <v-card-text>
+                    <div class="text-subtitle-1">
+                      <p>({{ vocabs[cardIdx]['pos'] }}.)</p>
+                      <p>Definition: {{ vocabs[cardIdx]['en_def'] }}</p>
+                      <p>中文定義： {{ vocabs[cardIdx]['ch_def'] }}</p>
+                      <p>Example: {{ vocabs[cardIdx]['en_example'] }}</p>
+                      <p>中文翻譯： {{ vocabs[cardIdx]['ch_example'] }}</p>
+                    </div>
+                  </v-card-text>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-container>
+          <v-card-actions class="justify-end">
+            <v-btn
+              text
+              @click="offset+=1"
+            >
+              Next Card
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-dialog
+      v-model="dialog"
+      max-width="700"
+    >
+      <v-card>
+        <v-toolbar
+          color="primary"
           dark
-          color="pink"
-        >
-          <v-toolbar-title>Vocabularies ({{cardIdx+1}}/{{boundMapper[selectedLevel]}})</v-toolbar-title>
-        </v-app-bar>
-
-        <v-container>
-          <v-row class="justify-center">
-            <v-col cols="12">
-              <v-card
-                class="ma-5"
-              >
-                <v-card-title class="text-h4">{{ vocabs[cardIdx]['headword'] }}</v-card-title>
-                <v-card-text>
-                  <div class="text-subtitle-1">
-                    <p>({{ vocabs[cardIdx]['pos'] }}.)</p>
-                    <p>Definition: {{ vocabs[cardIdx]['en_def'] }}</p>
-                    <p>中文定義： {{ vocabs[cardIdx]['ch_def'] }}</p>
-                    <p>Example: {{ vocabs[cardIdx]['en_example'] }}</p>
-                    <p>中文翻譯： {{ vocabs[cardIdx]['ch_example'] }}</p>
-                  </div>
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
+          class="text-h4"
+        >Congratulations</v-toolbar>
+        <v-card-text>
+          <div class="text-h5 pa-12">You've finished memorizing vocabular with {{levelMapper[selectedLevel]}} level</div>
+        </v-card-text>
         <v-card-actions class="justify-end">
           <v-btn
             text
-            @click="offset+=1"
-          >
-            Next Card
-          </v-btn>
+            @click="nextLevel"
+          >Next Level</v-btn>
         </v-card-actions>
       </v-card>
-    </v-col>
-  </v-row>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
@@ -111,6 +134,7 @@ export default {
   },
   data() {
     return {
+      dialog: false,
       selectedLevel: 0,
       previousLevel: 'A1',
       levelMapper: {
@@ -121,14 +145,6 @@ export default {
         4: 'C1',
         5: 'C2'
       },
-      items: [
-        { text: 'A1', icon: 'mdi-flag' },
-        { text: 'A2', icon: 'mdi-flag' },
-        { text: 'B1', icon: 'mdi-flag' },
-        { text: 'B2', icon: 'mdi-flag' },
-        { text: 'C1', icon: 'mdi-flag' },
-        { text: 'C2', icon: 'mdi-flag' },
-      ],
       offset: 0,
       boundMapper: {
         0: 498,
@@ -141,8 +157,29 @@ export default {
     }
   },
   computed: {
+    items() {
+      return [
+        { text: 'A1:  ' + this.startIdxMapper['A1'] + '/' + this.boundMapper[0], icon: 'mdi-flag' },
+        { text: 'A2:  ' + this.startIdxMapper['A2'] + '/' + this.boundMapper[1], icon: 'mdi-flag' },
+        { text: 'B1:  ' + this.startIdxMapper['B1'] + '/' + this.boundMapper[2], icon: 'mdi-flag' },
+        { text: 'B2:  ' + this.startIdxMapper['B2'] + '/' + this.boundMapper[3], icon: 'mdi-flag' },
+        { text: 'C1:  ' + this.startIdxMapper['C1'] + '/' + this.boundMapper[4], icon: 'mdi-flag' },
+        { text: 'C2:  ' + this.startIdxMapper['C2'] + '/' + this.boundMapper[5], icon: 'mdi-flag' },
+      ]
+    },
     cardIdx() {
-      return this.startIdx + this.offset
+      if (this.startIdx + this.offset < this.boundMapper[this.selectedLevel]) {
+        return this.startIdx + this.offset
+      } else {
+        this.dialog = true
+        return this.boundMapper[this.selectedLevel] - 1
+      }
+    }
+  },
+  methods: {
+    nextLevel() {
+      this.dialog = false
+      this.selectedLevel += 1
     }
   },
   watch: {
